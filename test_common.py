4bdb14e8c43855730c0e607372329f81780b12ba
James Jiang <jamesjiang52@gmail.com>
2020-10-10 14:09:01 -0400

    Add replace always block functionality

--- test_common.py
+++ test_common.py
@@ -1,5 +1,4 @@
-from common import pre_process
-from replace_with_logic import replace_with_logic
+from common import *
 
 #f_name = "verilog/divider_wrapper.v"
 #f_name = "verilog/divider.v"
@@ -9,4 +8,4 @@ f_name = "verilog/divider_datapath.v"
 with open(f_name, "r") as f:
     f_string = "\n".join(f.readlines())
 
-print(replace_with_logic(pre_process(f_string), debug=True))
+print(pre_process(f_string, debug=True))
